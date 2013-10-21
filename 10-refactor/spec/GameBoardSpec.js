febcb58947d99345ad43381878e2ace023af2eec
gemarodri <gemrodper@gmail.com>
2013-10-21 17:20:52 +0200

    añado Fireball

--- 10-refactor/spec/GameBoardSpec.js
+++ 10-refactor/spec/GameBoardSpec.js
@@ -40,3 +40,115 @@ Especificación: GameBoard debe
 
 
 */
+describe("GameBoard", function(){
+	
+	var canvas, ctx;
+
+    beforeEach(function(){
+	loadFixtures('index.html');
+
+	canvas = $('#game')[0];
+	expect(canvas).toExist();
+
+	ctx = canvas.getContext('2d');
+	expect(ctx).toBeDefined();
+
+    });
+
+	var game= new GameBoard();
+	it("GameBoard add", function(){
+		expect(game.add("5")).toEqual(game.objects[0]);
+	});
+
+	it("GameBoard removed", function(){
+		game.resetRemoved();
+		game.remove("5");
+		expect("5").toEqual(game.removed[0]);
+		game.finalizeRemoved("5");
+		expect(game.objects).toEqual([]);
+	});
+	it("GameBoard overlap", function(){
+		 var object= function (x, y, w, h){
+			this.x=x;
+			this.y=y;
+			this.w=w;
+			this.h=h;
+		};
+		var object1= new object(10,11,3,4);
+		var object2= new object(10,10,3,4);
+		var object3= new object (7,8,3,10);
+		expect(game.overlap(object1,object2)).toBe(true);
+		expect(game.overlap(object1,object3)).toBe(false);
+	});	
+
+	it("GameBoard step", function(){
+		spyOn(game, "step");
+		game.step("5");
+			expect(game.step).toHaveBeenCalled();	
+	});
+	
+	it("GameBoard draw", function(){
+		spyOn(game, "draw");
+		game.draw("ctx");
+		expect(game.draw).toHaveBeenCalled();
+	});
+	it("GameBoard collide", function(){
+		 var object= function ( x, y, w, h, type){
+			this.type=type;
+			this.x=x;
+			this.y=y;
+			this.w=w;
+			this.h=h;
+		};
+		var object1= new object(10,11,3,4, "1");
+		var object3= new object(10,10,3,4, "2");
+		var object2= new object( 7, 8, 3, 10);
+		
+		spyOn(game, "collide");
+		
+		expect(game.collide(object2)).toBe(game.objects[2]);
+		expect(game.collide(object2,"1")).toBeFalsy();
+	});
+	
+	it("GameBoard underscore iterate", function(){
+		var dummy1= {
+			step: function(){},
+		};
+		var dummy2= {
+			step: function(){},
+		};
+		
+		game.add(dummy1);
+		game.add(dummy2);
+		
+		spyOn(dummy1,"step");
+		spyOn(dummy2,"step");
+		
+		game.iterate("step", 1.0);
+		waits(100);
+        runs(function(){
+			expect(dummy1.step).toHaveBeenCalled();
+			expect(dummy2.step).toHaveBeenCalled();
+        });
+	});
+	it("GameBoard detect", function(){
+		
+		var dummy1= {
+			call: function(){},
+		};
+		var dummy2= {
+			call: function(){},
+		};	
+		game.add(dummy1);
+		game.add(dummy2);
+		spyOn(dummy1, "call"); 
+		spyOn(dummy2, "call");
+        game.detect(dummy1,dummy2);
+        waits(100);
+        runs(function(){
+			expect(game.objects[0]).toBeTruthy();
+			expect(game.objects[1]).toBeTruthy();
+        });
+		
+	});
+});
